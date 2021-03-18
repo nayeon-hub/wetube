8202e9698647ea18c5501c273e476c53c68d5b54
Park Na Yeon <within197@naver.com>
2021-03-18 23:52:04 +0900

    router and MVC

---
+++ routes.js
@@ -0,0 +1,18 @@
+//Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+//Users
+
+const USER = "/users";
+const USER_DETAIL = "/:id";
+const USER_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+//videos
+
+const VIDEOS = "/videos";
+const VIDEO_DETAIL = "/:id";
