2d76ff5ae500db54fb415229efb967b6f54491f6
Park Na Yeon <within197@naver.com>
2021-03-21 23:26:01 +0900

    wetube

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -14,8 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+import app from "./app";
+
 //Global
 const HOME = "/";
 const JOIN = "/join";
@@ -7,12 +9,34 @@ const SEARCH = "/search";
 
 //Users
 
-const USER = "/users";
+const USERS = "/users";
 const USER_DETAIL = "/:id";
-const USER_PROFILE = "/edit-profile";
+const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
 //videos
 
 const VIDEOS = "/videos";
+const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO,
+};
+
+export default routes;
