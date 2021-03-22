14cc2baf8f560dce7c60bacf9d5f71c030ec0821
Park Na Yeon <within197@naver.com>
2021-03-22 21:03:29 +0900

    wetube

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,9 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("Search");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Video" });
+export const upload = (req, res) => res.render("Upload");
+export const videoDetail = (req, res) => res.render("Video Detail");
+export const editVideo = (req, res) => res.render("Edit Video");
+export const deleteVideo = (req, res) => res.render("Delete Video");
