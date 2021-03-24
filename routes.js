47bfc246006db04fc56ae07a4bce9af1e3c9a965
Park Na Yeon <within197@naver.com>
2021-03-24 22:15:43 +0900

    wetube

--- routes.js
+++ routes.js
@@ -1,5 +1,3 @@
-import app from "./app";
-
 //Global
 const HOME = "/";
 const JOIN = "/join";
@@ -10,9 +8,9 @@ const SEARCH = "/search";
 //Users
 
 const USERS = "/users";
-const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
+const USER_DETAIL = "/:id";
 
 //videos
 
